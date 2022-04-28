1e3a253e880b225489be099eb129375ffe282028
Pavel Ostashkin <ostashpav@gmail.com>
2022-04-29 01:05:48 +0300

    chore: added eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,20 @@
+const { ESLint } = require('eslint');
+
+/**
+ * removes files that are mentionde in .eslintigonre
+ * @param {string[]} files staged files names
+ */
+const getFilesToLint = async (files) => {
+  const eslint = new ESLint();
+  const isIgnored = await Promise.all(files.map(file => eslint.isPathIgnored(file)));
+  return files.filter((_, index) => !isIgnored[index]);
+}
+
 module.exports = {
-  'src/**/*.(js|jsx|ts|tsx)': async (filenames) => {
+  'src/**/*.(js|ts)': async (filenames) => {
+    const filesToLint= await getFilesToLint(filenames);
     return [
+      `eslint ${filesToLint.join(' ')} --rule {no-console: 2, no-alert: 2, no-debugger: 2}`,
       `prettier ${filenames.join(' ')} -write`,
       'git add',
     ];
